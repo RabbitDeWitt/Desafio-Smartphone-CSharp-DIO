e57abe67f5f83d3a028ee0676881639560fec340
RabbitDeWitt <davidsocoelho@outlook.com>
2024-08-16 22:22:54 -0300

    first commit

---
+++ Models/Iphone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Smartphone_CSharp_DIO.Models
+{
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) { }
+
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando {nome} no Iphone");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Desafio_Smartphone_CSharp_DIO.Models;
+
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("123456", "Modelo 1", "11111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Iphone iphone= new Iphone("456789", "Modelo 2", "22222222", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
